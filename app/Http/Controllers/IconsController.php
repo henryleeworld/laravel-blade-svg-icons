4652344133e7d74b3129ee0a1dd14cbac34644dc
Henry Lee <henryleeworld@gmail.com>
2026-05-03 00:20:11 +0800

    feat:升級至 laravel 12

--- app/Http/Controllers/IconsController.php
+++ app/Http/Controllers/IconsController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 
 class IconsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         return view('icons');
